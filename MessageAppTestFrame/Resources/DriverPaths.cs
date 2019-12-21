235a77efd15222df4bd322e22c114910ae84a9b1
Elena <ellenatalpalaru@gmail.com>
2019-12-21 14:06:58 +0200

    Fixed driver path

--- MessageAppTestFrame/Resources/DriverPaths.cs
+++ MessageAppTestFrame/Resources/DriverPaths.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace MessageAppTestFrame.Resources
 {
     public static class DriverPaths
     {
-        public static string ChromeDriverPath = @"C:\Users\bogda\source\repos\MessageAppTestFrame\MessageAppTestFrame\Resources\";
+        //public static string ChromeDriverPath = @"C:\Users\bogda\source\repos\MessageAppTestFrame\MessageAppTestFrame\Resources\";
+        public static string ChromeDriverPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "../../../Resources");
     }
 }
